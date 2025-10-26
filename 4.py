61e2321dfe642933b525ec894c91383f29b76087
Admin <tctdat2007@gmail.com>
2025-10-26 20:51:35 +0700

    Write a function that gets a list of integers as a parameter

---
+++ 4.py
@@ -0,0 +1,10 @@
+def sum_list(numbers):
+    return sum(numbers)
+
+def main():
+    numbers = [1, 3, 5, 7, 9]
+    total = sum_list(numbers)
+    print(f"The sum of {numbers} is {total}")
+
+if __name__ == "__main__":
+    main()
